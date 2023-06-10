b3b46492825c43a5434086f1cc9673116d2b597a
Ed Scheinerman <ers@jhu.edu>
2023-06-10 11:56:28 -0400

    Setting up

---
+++ src/TicTocs.jl
@@ -0,0 +1,56 @@
+module TicTocs
+
+export TicToc, tic, toc
+
+g_start = 0.0
+
+
+"""
+    TicToc
+
+This is a timer. To start the timer:
+```
+t = TicToc();  # create a new timer 
+tic(t)         # starts the timer
+```
+Then use `toc(t)` to find out how many seconds have 
+elapsed since the timer was started.
+"""
+struct TicToc
+    start::Float64
+    TicToc() = new(0.0)
+end
+
+"""
+    tic()
+    tic(t::TicToc)
+
+Start a timer. With no argument, use the single global timer. 
+"""
+function tic()
+    global g_start = time()
+    nothing
+end
+
+function tic(t::TicToc)
+    t.start = time()
+    nothing
+end 
+
+
+"""
+    toc()
+    toc(t::TicToc)
+
+Return the number of seconds since the timer has been started.
+"""
+function toc()
+    global g_start
+    time() - g_start
+end
+
+function toc(t::TicToc)
+    time() - t.start
+end
+
+end # module TicTocs
